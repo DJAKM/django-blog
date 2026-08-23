42f2ebc91b49bc6c0e59cd53d9a54d264dc19d93
DJAKM <mishraaditya257@gmail.com>
2026-08-23 15:06:35 +0530

    global search features

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render ,get_object_or_404,redirect
 from django.http import HttpResponse
 # Create your views here.
 from blogs.models import Blog,Category
-
+from django.db.models import Q  
 def posts_by_category(request,category_id):
     # fetch the posts that belong to the category with the id category id
     posts = Blog.objects.filter(status=1 ,category=category_id)
@@ -25,3 +25,12 @@ def blogs(request,slug):
         'single_blog' : single_blog
     }
     return render(request , 'blogs.html',context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status=1,)
+    context ={
+        'blogs' : blogs,
+        'keyword': keyword,
+    }
+    return render(request,'search.html',context)
