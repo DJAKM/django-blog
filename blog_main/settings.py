10a67b68d3750fc63d42537818ac5dc35a0b5b12
DJAKM <mishraaditya257@gmail.com>
2026-09-12 11:09:04 +0530

    deployment(12-09): deployment changes for files

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e3_+wbmlae7me4yrq=rt(7#-y1q2glkaw1&cfxuo28fp_u-*d6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
